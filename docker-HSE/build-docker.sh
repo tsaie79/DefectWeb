9f50347368370a4548677af4d762e41b54f84364
jeng-yuantsai <tsaie79@gmail.com>
2023-09-29 17:13:18 -0400

    update

--- docker-HSE/build-docker.sh
+++ docker-HSE/build-docker.sh
@@ -4,5 +4,15 @@
 docker system prune -f
 docker rmi tsaie79/defectweb-grand:v0.1
 
-sh create-host-sshkey.sh
-docker build --build-arg PRIVATE_KEY=id_rsa-for-docker -t tsaie79/defectweb-grand:v0.1 .
+export HOST="localhost"
+# export HOST="129.10.50.43" db2
+
+
+# if $HOST is not "localhost" run the following command sh create-host-sshkey.sh
+if [ $HOST != "localhost" ]; then
+    sh create-host-sshkey.sh
+    export PRIVATE_KEY="./id_rsa-for-docker"
+fi
+
+
+docker build --build-arg PRIVATE_KEY=$PRIVATE_KEY -e HOST=$HOST -t tsaie79/defectweb-grand:v0.1 .
--- docker-HSE/tunnel-db.sh
+++ docker-HSE/tunnel-db.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export HOST="129.10.50.43"
 echo -n "Creating SSH tunnel to $HOST... "
 ssh -4 -q -N -f -T -M -L localhost:12349:localhost:27017 tsai@$HOST
 echo "Done!"
