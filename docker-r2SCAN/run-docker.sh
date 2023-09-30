c70d90388089ae30605bc9cb7e236d87b8ed49c6
jeng-yuantsai <tsaie79@gmail.com>
2023-09-30 13:44:23 -0400

    update

--- docker-r2SCAN/run-docker.sh
+++ docker-r2SCAN/run-docker.sh
@@ -7,5 +7,5 @@ docker system prune -f
 
 export HOST="129.10.50.43"
 
-docker run -it -d -p 127.0.0.1:5000:5000 -e HOST=$HOST --name defectweb-r2scan tsaie79/defectweb-r2scan:v0.1 bash
+docker run -d -p 127.0.0.1:5000:5000 -e HOST=$HOST --name defectweb-r2scan tsaie79/defectweb-r2scan:v0.1
 
