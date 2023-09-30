5625edc2ac70abb43809c826d5d7916a5a546578
jeng-yuantsai <tsaie79@gmail.com>
2023-09-30 14:55:18 -0400

    fix

--- docker-r2SCAN/run-docker.sh
+++ docker-r2SCAN/run-docker.sh
@@ -7,5 +7,5 @@ docker system prune -f
 
 export HOST="129.10.50.43"
 
-docker run -itd -p 127.0.0.1:5000:5000 -e HOST=$HOST -v r2SCAN/static/materials:/usr/local/lib/python3.7/site-packages/flamyngo/static/materials --name defectweb-r2scan defectweb-r2scan:v0.1
+docker run -itd -p 127.0.0.1:5000:5000 -e HOST=$HOST -v r2SCAN/static/materials:/usr/local/lib/python3.7/site-packages/flamyngo/static/materials --name defectweb-r2scan tsaie79/defectweb-r2scan:v0.1
 
