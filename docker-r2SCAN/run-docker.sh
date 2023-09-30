a3f5698c2a08adc1db9cad097c3184506a0e0e13
jeng-yuantsai <tsaie79@gmail.com>
2023-09-30 14:57:50 -0400

    add env about mount point in host

--- docker-r2SCAN/run-docker.sh
+++ docker-r2SCAN/run-docker.sh
@@ -6,6 +6,7 @@ docker stop defectweb-r2scan
 docker system prune -f
 
 export HOST="129.10.50.43"
+export RAWDATA_MOUNT_POINT="/home/tsai/defectweb/DefectWeb/docker-r2SCAN/r2SCAN/static/materials"
 
-docker run -itd -p 127.0.0.1:5000:5000 -e HOST=$HOST -v r2SCAN/static/materials:/usr/local/lib/python3.7/site-packages/flamyngo/static/materials --name defectweb-r2scan tsaie79/defectweb-r2scan:v0.1
+docker run -itd -p 127.0.0.1:5000:5000 -e HOST=$HOST -v $RAWDATA_MOUNT_POINT:/usr/local/lib/python3.7/site-packages/flamyngo/static/materials --name defectweb-r2scan tsaie79/defectweb-r2scan:v0.1
 
