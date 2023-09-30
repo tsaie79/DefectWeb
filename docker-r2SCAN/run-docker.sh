cba7eb294ce6d7db561ceda398513e4c5cf3b769
jeng-yuantsai <tsaie79@gmail.com>
2023-09-30 14:49:46 -0400

    mount static/materials

--- docker-r2SCAN/run-docker.sh
+++ docker-r2SCAN/run-docker.sh
@@ -7,5 +7,6 @@ docker system prune -f
 
 export HOST="129.10.50.43"
 
-docker run -itd -p 127.0.0.1:5000:5000 -e HOST=$HOST --name defectweb-r2scan tsaie79/defectweb-r2scan:v0.1 /bin/bash
+docker run -itd -p 127.0.0.1:5000:5000 -e HOST=$HOST -v r2SCAN/static/materials:/usr/local/lib/python3.7/site-packages/flamyngo/static/materials \ 
+--name defectweb-r2scan defectweb-r2scan:v0.1
 
