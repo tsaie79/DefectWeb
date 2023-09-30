16888ea2302a6778629386f5afddce8caead886f
jeng-yuantsai <tsaie79@gmail.com>
2023-09-30 13:57:36 -0400

    run docker image in background

--- docker-HSE/run-docker.sh
+++ docker-HSE/run-docker.sh
@@ -7,5 +7,5 @@ docker system prune -f
 
 export HOST="129.10.50.43"
 
-docker run -it -d -p 127.0.0.1:5000:5000 -e HOST=$HOST --name defectweb-hse tsaie79/defectweb-hse:v0.1 bash
+docker run -itd -p 127.0.0.1:5000:5000 -e HOST=$HOST --name defectweb-hse tsaie79/defectweb-hse:v0.1 /bin/bash
 
